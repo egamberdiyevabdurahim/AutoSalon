a99b1b1e35d44d69cdaa4dcd3901fc71ee183207
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-08-25 13:01:32 +0500

    fixed

--- db_settings.py
+++ db_settings.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2.extras import DictCursor
+from psycopg2 import OperationalError
 
 from config import DB_CONFIG
 
@@ -10,44 +11,64 @@ class Database:
         self.conn = None
 
     def __enter__(self):
-        # connect to the database
-        self.conn = psycopg2.connect(**DB_CONFIG)
-        self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+        try:
+            # Connect to the database
+            self.conn = psycopg2.connect(**DB_CONFIG)
+            self.cursor = self.conn.cursor()
+        except OperationalError as e:
+            print(f"Error connecting to the database: {e}")
+            raise e
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # close the cursor and connection
         if exc_type is not None:
             self.conn.rollback()
         else:
             self.conn.commit()
 
-        if self.conn is not None:
-            self.conn.close()
-
+        # Close cursor and connection
         if self.cursor is not None:
             self.cursor.close()
 
+        if self.conn is not None:
+            self.conn.close()
+
     def execute(self, query: str, params: tuple = None):
         """
-        Execute a SQL query with parameters.
+        Execute a SQL query with optional parameters.
         """
-        self.cursor.execute(query, params)
-        self.cursor.commit()
+        try:
+            self.cursor.execute(query, params)
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            self.conn.rollback()
+            raise e
 
     def fetchone(self, query: str, params: tuple = None):
         """
         Execute a SQL query and fetch one row.
         """
-        self.cursor.execute(query, params)
+        self.execute(query, params)
         return self.cursor.fetchone()
 
     def fetchall(self, query: str, params: tuple = None):
         """
         Execute a SQL query and fetch all rows.
         """
-        self.cursor.execute(query, params)
+        self.execute(query, params)
         return self.cursor.fetchall()
+
+    def commit(self):
+        """
+        Manually commit the current transaction.
+        """
+        self.conn.commit()
+
+    def rollback(self):
+        """
+        Manually rollback the current transaction.
+        """
+        self.conn.rollback()
 
 
 def execute_query(query, params: tuple = None, fetch: str=None):
